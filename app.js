13938d54e5b955a32e00b6aff55d8f6558b7759a
Dmitriy Angve <kirirch129@gmail.com>
2023-01-15 14:49:19 +0300

    #

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
 const output = document.querySelector(".output");
 console.log(output);
-output.textContent = "New content";
+// output.textContent = "New content";
 const url = "list.json";
-
+let myList = [];
+let localData = localStorage.getItem("myList");
+console.log(localData);
 window.addEventListener("DOMContentLoaded", () => {
   output.textContent = "Loading...";
-  fetch(url)
-    .then((rep) => rep.json())
-    .then((data) => {
-      console.log(data);
-      output.innerHTML = "";
-      data.forEach((el) => {
-        makeList(el);
+
+  if (localData) {
+    myList = JSON.parse(localStorage.getItem("myList"));
+    console.log(myList);
+    maker();
+  } else {
+    fetch(url)
+      .then((rep) => rep.json())
+      .then((data) => {
+        myList = data;
+        maker();
+        localStorage.setItem("myList", JSON.stringify(myList));
       });
-    });
+  }
 });
 
-function makeList(item) {
+function maker() {
+  output.innerHTML = "";
+  myList.forEach((el, index) => {
+    makeList(el, index);
+  });
+}
+
+function makeList(item, index) {
   const div = document.createElement("div");
   div.innerHTML = `${item.name} #(${item.guests})`;
   output.append(div);
@@ -26,4 +40,17 @@ function makeList(item) {
   } else {
     div.classList.add("notConfirmed");
   }
+  div.addEventListener("click", (e) => {
+    div.classList.toggle("confirmed");
+    div.classList.toggle("notConfirmed");
+    console.log(div.classList.contains("confirmed"));
+
+    if (div.classList.contains("confirmed")) {
+      myList[index].status = true;
+    } else {
+      myList[index].status = false;
+    }
+    localStorage.setItem("myList", JSON.stringify(myList));
+    console.log(myList);
+  });
 }
